7ef78d9c3edb8cf423eb0d349a0fdff37bb04dd3
Ben_Puls <discordben7@gmail.com>
2024-02-24 16:27:27 +0500

    update

---
+++ parser/example/find.cs
@@ -0,0 +1,12 @@
+﻿using Parser;
+
+string path = $"{Environment.CurrentDirectory.ToString()}\\example\\data.cfg";
+Console.WriteLine();
+string MyKey = Cfg.GetValue(@path, "name");
+
+Console.WriteLine(MyKey);
+
+// --------------------------------------------
+// Rewrite file
+
+Cfg.NewValue(@path, "name", "GGWP Loosers");
---
+++ parser/main.cs
@@ -0,0 +1,63 @@
+﻿using System.IO;
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Parser
+{
+
+
+    class Cfg
+    {
+        public static string GetValue(string path, string key)
+        {
+            string result = "";
+            using (StreamReader reader = new StreamReader(path, Encoding.UTF8))
+            {
+                Regex regex = new Regex("\\b" + key + "\\b", RegexOptions.IgnoreCase);
+                string str = null;
+                while ((str = reader.ReadLine()) != null)
+                {
+                    if (regex.IsMatch(str))
+                    {
+                        result = str;
+                        break;
+                    }
+                }
+            }
+            try
+            {
+                return result.Split("\"")[1];
+            }
+            catch
+            {
+                return "Unable to read file";
+            }
+        }
+
+        public static void NewValue(string path, string key, string value)
+        {
+            int CountLine = 0;
+            string result = "";
+            using (StreamReader reader = new StreamReader(path, Encoding.UTF8))
+            {
+                Regex regex = new Regex("\\b" + key + "\\b", RegexOptions.IgnoreCase);
+                string str = null;
+                while ((str = reader.ReadLine()) != null)
+                {
+                    CountLine++;
+                    if (regex.IsMatch(str))
+                    {
+                        result = str;
+                        //Console.WriteLine(CountLine);
+                        break;
+                    }
+                }
+            }
+            string AllData = File.ReadAllText(path);
+            string NewData = AllData.Replace(result, $"name \"{value}\"");
+            File.WriteAllText(path, NewData);
+            
+        }
+    }
+}
